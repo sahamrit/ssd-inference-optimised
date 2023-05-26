cb84504dba7396bd6dba03bf1aeb920723d6cf30
sahamrit <amritsahu.iitkgp@gmail.com>
2023-05-26 06:21:23 +0000

    Convert GstBuf to np array

--- gst_frame_probe.py
+++ gst_frame_probe.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 import gi
+import numpy as np
 
 gi.require_version("Gst", "1.0")
 
@@ -16,10 +17,40 @@ logger = logging.getLogger(__name__)
 videoformat = "RGBA"
 
 
+def get_buffer_size(caps):
+    caps_struct = caps.get_structure(0)
+
+    (success, width) = caps_struct.get_int("width")
+    if not success:
+        return False, (0, 0)
+
+    (success, height) = caps_struct.get_int("height")
+    if not success:
+        return False, (0, 0)
+
+    return True, (width, height)
+
+
 def probe_callback_per_frame(pad, info):
     logger.debug(f"probe id - {info.id}")
     buf = info.get_buffer()
     logger.debug(f"buffer pts - [{buf.pts / Gst.SECOND:6.2f} sec]")
+    success, map_info = buf.map(Gst.MapFlags.READ)
+
+    if not success:
+        raise RuntimeError("Could not map buffer data!")
+
+    success, (width, height) = get_buffer_size(pad.get_current_caps())
+
+    if not success:
+        raise RuntimeError("Could not extract widht and height from pad caps!")
+
+    logger.info(f"Extracted buffer of shape (H / W ) ({height} / {width})")
+    numpy_frame = np.ndarray(
+        shape=(height, width, 4), dtype=np.uint8, buffer=map_info.data
+    )
+    logger.info(f"Converted to numpy array of shape - {numpy_frame.shape}")
+
     return Gst.PadProbeReturn.OK
 
 
@@ -31,7 +62,7 @@ pipeline = Gst.parse_launch(
     f"filesrc location=media/in.mp4 ! \
      decodebin ! \
      nvvideoconvert ! \
-     video/x-raw(memory:NVMM), format = {videoformat} ! \
+     video/x-raw, format = {videoformat} ! \
      fakesink name=fs"
 )
 
